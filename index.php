4ab8522d2b07c894ccfd5bcaec96c808b0ce744e
jritas <yiannis.ritas@gmail.com>
2025-12-04 17:25:03 +0200

    commit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Front controller για Plesk: προωθεί όλα τα requests
-// στο κανονικό public/index.php της εφαρμογής.
-require __DIR__ . '/public/index.php';
--- public/dbcheck.php
+++
@@ -1,4 +0,0 @@
-<?php
-declare(strict_types=1);
-require __DIR__ . '/../app/Config/db.php';
-echo $pdo->query('SELECT 1')->fetchColumn() ? 'DB OK' : 'DB FAIL';
